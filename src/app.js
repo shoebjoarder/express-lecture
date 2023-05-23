b3c35ed9cbd1ce681a37534637c322f36535b8ad
Shoeb Joarder <shoebjoarder@outlook.com>
2023-05-23 22:14:06 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 // app.js
 const express = require("express");
 
+const router = require("./routes/auth.routes");
+
 PORT = 3000;
 
 const app = express();
+
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
